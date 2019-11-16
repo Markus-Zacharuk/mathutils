d0e20de590e7e09d4cce69c4a352b39b65ea3a13
Markus-Zacharuk <57815046+Markus-Zacharuk@users.noreply.github.com>
2019-11-16 21:30:44 +0100

    Update extended_euclidean_algorithm.py

--- extended_euclidean_algorithm.py
+++ extended_euclidean_algorithm.py
@@ -1,6 +1,6 @@
 """ Implementation of the extended Euclidean algorithm that prints out all steps in the form of a matrix.
-"""
-""" https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm:
+
+https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm:
 'In arithmetic and computer programming, the extended Euclidean algorithm is an extension to the Euclidean
 algorithm, and computes, in addition to the greatest common divisor of integers a and b, also the coefficients
 of Bézout's identity, which are integers x and y such that [a*s+b*t=gcd(a,b)].'
@@ -9,12 +9,14 @@ of Bézout's identity, which are integers x and y such that [a*s+b*t=gcd(a,b)].'
 __author__ = "Zacharuk, Markus"
 __email__ = "markus.zacharuk@stud.uni-frankfurt.de"
 
-def create_string(table, space_between_columns = 2):
+
+def create_string(table, space_between_columns=2):
     """ This creates a formatted string with variable column widths.
+
     Printing with "\t" didn't format well when big numbers were given, so this is a workaround
     """
     result = ""
-    column_widths = [0,0,0,0,0]
+    column_widths = [0, 0, 0, 0, 0]
     for i in range(len(table)):
         for j in range(1, 5):
             if column_widths[j] < len(str(table[i][j-1])) + space_between_columns:
@@ -33,8 +35,10 @@ def create_string(table, space_between_columns = 2):
         result += "\n"
     return result
 
+
 def extended_euclidean_algorithm(a, b):
     """ Clculates the biggest common divisor with bézout-coefficients.
+
     Prints out a matrix that shows each step of the algorithm.
     a and b can be any integers.
     """
@@ -43,15 +47,15 @@ def extended_euclidean_algorithm(a, b):
     s = 0
     t = 0
     q = 0
-    
-    if b>a:
+
+    if b > a:
         # Make sure the numbers are given in the right order to the algorithm
-        c=a
-        a=b
-        b=c
+        c = a
+        a = b
+        b = c
     # The list (matrix) representing each step as sublist. the first element (and row) is the header, specifying the columns.
-    table = [['q','r','s','t'],[0,a,1,0],[a//b,b,0,1]]
-    
+    table = [['q', 'r', 's', 't'], [0, a, 1, 0], [a//b, b, 0, 1]]
+
     while True:
         # The algorithm-loop
         r = table[-2][1] - table[-1][0] * table[-1][1]
@@ -60,13 +64,14 @@ def extended_euclidean_algorithm(a, b):
         s = table[-2][2] - table[-1][0] * table[-1][2]
         t = table[-2][3] - table[-1][0] * table[-1][3]
         q = table[-1][1] // r
-        table.append([q,r,s,t])
+        table.append([q, r, s, t])
 
     print()
     print(create_string(table))
 
-    print("gcd(",a,",",b,") = ", table[-1][1], sep="")
-    print(s,"*",a,"+",t,"*",b,"=",s*a + t*b, end="\n\n")
+    print("gcd(", a, ",", b, ") = ", table[-1][1], sep="")
+    print(s, "*", a, "+", t, "*", b, "=", s*a + t*b, end="\n\n")
+
 
 def parse_input():
     """ Get two integers from the user as input and return them"""
@@ -84,6 +89,7 @@ def parse_input():
     b = int(input_str)
     return a, b
 
+
 def main():
     """ Handle the controll flow of the program: Get the numbers and run the algorithm on them."""
     print("Implementation of the extended Euclidean algorithm that prints out all steps in the form of a matrix.\nType exit to end the program.")
@@ -91,8 +97,9 @@ def main():
         a, b = parse_input()
         if a == 0 and b == 0:
             break
-        extended_euclidean_algorithm(a,b)
+        extended_euclidean_algorithm(a, b)
     print("Program exited.")
+
 
 if __name__ == "__main__":
     main()
